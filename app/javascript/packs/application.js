8e21847069e5fffa2658df8af5faa5a4ae711472
Tovis Bratsburg <tovisbratsburg@gmail.com>
2021-10-19 09:18:38 -0400

    dropdown

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,13 +1,13 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import '@doabit/semantic-ui-sass'
+import 'jquery'
+
+$(document).on('turbolinks:load', function() {
+    $('.ui.dropdown').dropdown();
+})
 
 Rails.start()
 Turbolinks.start()
