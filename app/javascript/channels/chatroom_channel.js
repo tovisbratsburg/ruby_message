6df0f773b3759797da701a073f15d60eccf97c99
Tovis Bratsburg <tovisbratsburg@gmail.com>
2021-10-28 12:21:59 -0400

    final styling

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -10,6 +10,7 @@ consumer.subscriptions.create("ChatroomChannel", {
   },
 
   received(data) {
-    // Called when there's incoming data on the websocket for this channel
+    $('#message-container').append(data.mod_message)
+    scroll_bottom()
   }
 });
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,11 +5,27 @@ import "channels"
 import '@doabit/semantic-ui-sass'
 import 'jquery'
 
+scroll_bottom = function() {
+    if ($('#messages').length > 0) {
+        $('#messages').scrollTop($('#messages')[0].scrollHeight);
+    }
+}
+
+submit_message = function() {
+    $('#message_body').on('keydown', function(e) {
+        if (e.keyCode == 13) {
+            $('button').click();
+            e.target.value = "";
+        };
+    });
+};
+
 $(document).on('turbolinks:load', function() {
     $('.ui.dropdown').dropdown();
     $('.message .close').on('click', function() {
         $(this).closest('.message').transition('fade');
     });
+    scroll_bottom();
 })
 
 Rails.start()
